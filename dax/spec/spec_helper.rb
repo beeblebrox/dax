1db665173e6557046d74c8dc889f6d9d208c595e
James <beeblebrox42@gmail.com>
2014-02-17 18:32:53 -0500

    Adds set_contain_file_named matcher for diff sets.

--- dax/spec/spec_helper.rb
+++ dax/spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'dax'
+require 'rspec/expectations'
 
+RSpec::Matchers.define :set_contain_file_named do |name|
+  match do |set|
+     true if set.to_a.index do |fileHash|
+      fileHash[:name] == name
+    end
+  end
+end
