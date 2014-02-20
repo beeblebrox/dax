8c171379750e92672bff445bcfc58d12cea7efdb
James N. Hart <beeblebrox42@gmail.com>
2014-02-19 23:33:18 -0700

    Check for errors and cleanly shutdown.

--- dax/lib/server.rb
+++ dax/lib/server.rb
@@ -22,13 +22,9 @@ class Server < Logger::Application
     @zmq = ZMQ::Context.new(50)
     @socket = @zmq.socket(ZMQ::REP)
     @socket.bind(@listen)
-
+    Thread::abort_on_exception = true
     @zthread = Thread.new do
-      msg = ''
-      puts "reading"
-      @socket.recv_string msg rescue puts $!.inspect, $@
-      puts "writing"
-      @socket.send_string msg rescue puts $!.inspect, $@
+      server_loop
     end
   end
 
@@ -40,13 +36,31 @@ class Server < Logger::Application
   end
 
   def error_check(rc)
-    if ZMQ::Util.resultcode_ok?(rc)
+    if ZMQ::Util.resultcode_ok?(rc) || ZMQ::Util.errno ==  ZMQ::EAGAIN
       false
     else
-      STDERR.puts "Operation failed, errno [#{ZMQ::Util.errno}] description [#{ZMQ::Util.error_string}]"
-      caller(1).each { |callstack| STDERR.puts(callstack) }
+      log ERROR, "Operation failed, errno [#{ZMQ::Util.errno}] description [#{ZMQ::Util.error_string}]"
       true
     end
   end
   
+  private
+
+  def server_loop
+    @socket.setsockopt(ZMQ::LINGER, 1000)
+    @socket.setsockopt(ZMQ::RCVTIMEO, 1000)
+    until @shutdown
+      msg = ''
+      begin 
+        rc = @socket.recv_string msg
+        log DEBUG, "Received: #{msg}" unless rc < 0
+        break if @shutdown
+      end while rc < 0 && ZMQ::Util.errno == ZMQ::EAGAIN
+      raise "Could not receive message." if error_check(rc)
+      rc = @socket.send_string msg
+      raise "Could not send message." if error_check(rc)
+    end
+    rescue
+      log FATAL, "Error while running server, shutting down. (#{$!.inspect})"
+  end
 end
