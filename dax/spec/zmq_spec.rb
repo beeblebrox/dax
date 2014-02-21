3bb1e5f073fbb7ee86bf67dd6eeac049edd663ae
James N. Hart <beeblebrox42@gmail.com>
2014-02-21 00:01:55 -0700

    Testing additional assumptions.

--- dax/spec/zmq_spec.rb
+++ dax/spec/zmq_spec.rb
@@ -20,4 +20,42 @@ describe ZMQ do
     expect(ZMQ::Util.errno).to be == ZMQ::EAGAIN
   end
   
+  
+  it "always provides EAGAIN for RECV timeout" do
+    zmq = ZMQ::Context.new(50)
+    socket = zmq.socket(ZMQ::REP)
+    socket.bind('tcp://*:4242')
+    
+    socket.setsockopt(ZMQ::LINGER, 1000)
+    socket.setsockopt(ZMQ::RCVTIMEO, 1000)
+    # Will timeout due to socket option
+    msg = ''
+    Thread.new do
+      5.times do
+        rc = socket.recv_string msg
+        expect(rc).to be == -1
+        # As far as I can tell, this should be the correct error
+        expect(ZMQ::Util.errno).to be == ZMQ::EAGAIN
+      end
+    end.join
+  end
+
+ it "always provides same errorno" do
+    zmq = ZMQ::Context.new(50)
+    socket = zmq.socket(ZMQ::REP)
+    socket.bind('tcp://*:4242')
+    
+    socket.setsockopt(ZMQ::LINGER, 1000)
+    socket.setsockopt(ZMQ::RCVTIMEO, 1000)
+    # Will timeout due to socket option
+    msg = ''
+    rc = socket.recv_string msg
+    expect(rc).to be == -1
+    # As far as I can tell, this should be the correct error
+    expect(ZMQ::Util.errno).to be == ZMQ::EAGAIN
+    # This is suprising if it fails
+    100.times do
+      expect(ZMQ::Util.errno).to be == ZMQ::EAGAIN
+    end
+  end
 end
