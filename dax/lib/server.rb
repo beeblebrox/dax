fc72745cef75ee1aa93afa7cc4ed0a46b9b20c00
James N. Hart <beeblebrox42@gmail.com>
2014-02-22 00:41:59 -0700

    Add encryption support to server.

--- dax/lib/server.rb
+++ dax/lib/server.rb
@@ -18,7 +18,7 @@ class Server < Logger::Application
     @db = options[:db]
     @listen = options[:listen]
     @key = options[:key]
-    @zmq = ZMQ::Context.new(50)
+    @zmq = ZMQ::Context.new(1)
     @socket = @zmq.socket(ZMQ::REP)
     @socket.bind(@listen)
     Thread::abort_on_exception = true
@@ -29,9 +29,9 @@ class Server < Logger::Application
   end
 
   def cleanup
-    raise @error if @error
     @shutdown = true
-    @zthread.join
+    raise @error if @error
+    @zthread.join if @zthread
   end
 
   def context
@@ -50,7 +50,7 @@ class Server < Logger::Application
   private
 
   def server_loop
-    @socket.setsockopt(ZMQ::LINGER, 1000)
+    @socket.setsockopt(ZMQ::LINGER, 0)
     @socket.setsockopt(ZMQ::RCVTIMEO, 1000)
     until @shutdown
       msg = ''
@@ -58,12 +58,14 @@ class Server < Logger::Application
       errno = 0
       begin 
         rc = @socket.recv_string msg
-        log DEBUG, "Received: #{msg}" unless rc < 0
         errno = ZMQ::Util.errno
+        log DEBUG, "Received: #{msg}" unless rc < 0
         break if @shutdown
       end while rc < 0 && errno == ZMQ::EAGAIN
       break if @shutdown
       raise "Could not receive message." if error_check(rc, errno)
+      msg = Crypto.decrypt(@key, msg) if @key
+      msg = Crypto.encrypt(@key, msg) if @key
       rc = @socket.send_string msg
       errno = ZMQ::Util.errno
       raise "Could not send message." if error_check(rc, errno)
