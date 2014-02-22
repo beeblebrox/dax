128c45bae465cc31a95574b76dca2bf08b6e2611
James N. Hart <beeblebrox42@gmail.com>
2014-02-22 00:42:09 -0700

    Adds encryption enabled server test.
    This also fixes a testing issue where zeromq  can't seem to handle creating multiple
    client or server endpoints at the same location with different contexts.
    
    Can't tell where the issue might be, probably should submit spec test to
    ffi-rzmq devs.

--- dax/spec/server_spec.rb
+++ dax/spec/server_spec.rb
@@ -10,12 +10,54 @@ describe Server do
       socket = zmq.socket(ZMQ::REQ)
       socket.setsockopt(ZMQ::RCVTIMEO, 1000)
       socket.setsockopt(ZMQ::SNDTIMEO, 1000)
+      socket.setsockopt(ZMQ::LINGER, 0)
       socket.connect  "tcp://127.0.0.1:4242"
       socket.send_string "yomammy"
       msg = ''
       rc = socket.recv_string msg
       expect(ZMQ::Util.resultcode_ok?(rc)).to be
       expect(msg).to be == "yomammy"
+    ensure
+     s.cleanup if s
+    end
+  end
+  
+  it "creates zmq server, again" do
+    begin
+      s = Server.new :db => nil, :listen => "tcp://*:4243", :key => nil
+      zmq = ZMQ::Context.new
+      socket = zmq.socket(ZMQ::REQ)
+      socket.setsockopt(ZMQ::RCVTIMEO, 1000)
+      socket.setsockopt(ZMQ::SNDTIMEO, 1000)
+      socket.setsockopt(ZMQ::LINGER, 0)
+      socket.connect  "tcp://127.0.0.1:4243"
+      socket.send_string "yomammy"
+      msg = ''
+      rc = socket.recv_string msg
+      expect(ZMQ::Util.resultcode_ok?(rc)).to be
+      expect(msg).to be == "yomammy"
+    ensure
+     s.cleanup if s
+    end
+  end
+  
+  it "encrypted zmq server" do
+    begin
+      s = Server.new :db => nil, :listen => "tcp://*:4244", :key => "mysecret"
+      zmq = ZMQ::Context.new
+      socket = zmq.socket(ZMQ::REQ)
+      socket.setsockopt(ZMQ::RCVTIMEO, 1000)
+      socket.setsockopt(ZMQ::SNDTIMEO, 1000)
+      socket.connect  "tcp://127.0.0.1:4244"
+      original_cipher = Crypto.encrypt("mysecret", "yomammy")
+      socket.send_string original_cipher 
+      msg = ''
+      rc = socket.recv_string msg
+      # Encryption should use different iv each time, thus different cipher result
+      # for same values.
+      expect(Crypto.decrypt("mysecret", msg)).to eq "yomammy"
+      expect(msg).not_to eq original_cipher
+      expect(ZMQ::Util.resultcode_ok?(rc)).to be
     ensure
      s.cleanup if s
     end
